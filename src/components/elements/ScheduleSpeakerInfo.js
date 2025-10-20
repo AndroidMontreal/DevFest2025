f7b115fa581a7b75d5811d5e21885453a8e7515a
Solankimimoh <solankimimoh@gmail.com>
2025-10-20 00:32:13 -0400

    Fixed hover effect on schedule's speaker

--- src/components/elements/ScheduleSpeakerInfo.js
+++ src/components/elements/ScheduleSpeakerInfo.js
@@ -11,18 +11,18 @@ const ScheduleSpeakerInfo = ({ speaker, locale = 'en' }) => {
   return (
     <Link
       href={`/${locale}/speakers/${slug}`}
-      className="flex items-center gap-3 mt-1 rounded-md  p-2 -ml-2 transition-all duration-200 group "
+      className="flex items-center gap-3 mt-1 rounded-md  p-2 -ml-2 transition-all duration-200 group/a "
       title={`${name} - ${title}`}
     >
       <Image
-        className="h-11 w-11 rounded shadow object-cover group-hover:shadow-md group-hover:scale-[105%]  transition-all duration-200 ease-in"
+        className="h-11 w-11 rounded shadow object-cover group-hover/a:shadow-md group-hover/a:scale-[105%]  transition-all duration-200 ease-in"
         src={image}
         alt={name}
         width={44}
         height={44}
       />
       <div>
-        <p className="text-base font-semibold tracking-normal text-gray-800 group-hover:text-blue-800 transition-colors duration-200">
+        <p className="text-base font-semibold tracking-normal text-gray-800 group-hover/a:text-blue-800 transition-colors duration-200">
           {name}
         </p>
         <p className="text-xs text-gray-500">{title}</p>
